26689339beb9b2066b0d49da5616813d21336d04
Basir Mohammadi <basirahmadmohammadi852@gmail.com>
2022-07-18 22:29:20 +0100

    Conditional statement

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ class App extends Component {
   state = {
     ninjas : [
       {name: 'ahmad', age:30, belt:'black', id:1},
-      {name: 'ahmad', age:30, belt:'black', id:2},
-      {name: 'ahmad', age:30, belt:'black', id:3}
+      {name: 'ali', age:21, belt:'black', id:2},
+      {name: 'akbar', age:25, belt:'black', id:3}
     ]
   }
 render() {
--- src/ninjas.js
+++ src/ninjas.js
@@ -1,14 +1,29 @@
 const Ninjas = ({ninjas}) => {
     // const {ninjas} =props;
     const ninjaList = ninjas.map(ninja => {
-        return ( 
-            <div className="ninja" key={ninja.id}>
-                <div>Name:{ninja.name}</div>
-                <div>Age:{ninja.age}</div>
-                <div>Belt:{ninja.belt}</div>
-            </div>
-         );
+        if(ninja.age<25) {
+            return ( 
+                <div className="ninja" key={ninja.id}>
+                    <div>Name:{ninja.name}</div>
+                    <div>Age:{ninja.age}</div>
+                    <div>Belt:{ninja.belt}</div>
+                </div>
+             );
+        }else {
+            return null
+        }
+        
     });
+
+    // const ninjaList = ninjas.map( ninja => {
+    //     return ninja.age<25 ? (
+    //         <div className="ninja" key={ninja.id}>
+    //         <div>Name:{ninja.name}</div>
+    //          <div>Age:{ninja.age}</div>
+    //           <div>Belt:{ninja.belt}</div>
+    //             </div>
+    //      ) : null;
+    // })
     return (
         <div>
             {ninjaList}
